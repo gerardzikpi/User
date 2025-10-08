08dddc06d80a8398fc7a70f52c6333fdab73d2d8
Gerard Zikpi <kodinapetra@yahoo.com>
2025-10-08 02:47:36 -0700

    added gender description

--- coreuser/models.py
+++ coreuser/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+GENDER_CHOICES=[("MALE","male"),
+              ("FEMALE","female")]
+
 class CoreUser(AbstractBaseUser):
     name = models.TextField(max_length = 25)
     username = models.CharField(max_length =25)
     email = models.EmailField()
     profilephoto = models.ImageField(upload_to= 'images')
     birthdate = models.DateField()
+    gender = models.ChoiceField(choices = GENDER_CHOICES)
 
 
     def __str__(self):
